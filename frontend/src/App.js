717f508625b176e7269995ef0bc7a2f325d37375
Kerun Pan <pantheon19970707@gmail.com>
2021-10-10 14:41:49 -0400

    Build: add .gitgnore, git init, add readme template, add frontend and backend dev docker env,  set up local docker env properly, add circleci template

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+
+const App = () => (
+    <h1>
+        ????
+    </h1>
+);
+
+export default App;
