fee1cb50db7a30c5ae6e1d108c73dd51edd30361
KerunPan-uwindsor <pan16@uwindsor.ca>
2021-10-11 16:51:38 -0400

    fix: add unity build into frontend public, unity game is working inside react components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,31 @@
+//dependencies
 import React from "react";
-import './App.css';
 import {Provider} from 'react-redux';
+import Unity, { UnityContext } from "react-unity-webgl";
 
+//local
+import './App.css';
 import Bugs from './components/Bugs';
 import configureStore from './store/configureStore';
 
 const store = configureStore();
 
+const buildUrl = "Unity_WebGL_Build";
+
+const unityContext = new UnityContext({
+    loaderUrl: `${buildUrl}/Builds.loader.js`,
+    dataUrl: `${buildUrl}/Builds.data`,
+    frameworkUrl: `${buildUrl}/Builds.framework.js`,
+    codeUrl: `${buildUrl}/Builds.wasm`,
+  });
+
 function App() {
   return (
     <Provider store={store}>
-      <Bugs/>
+      {/* <Bugs/> */}
       <h1>1111</h1>
+      <Unity unityContext={unityContext} />
     </Provider>
-    
   );
 }
 
