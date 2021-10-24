20884bf966e89c5c9f4ca8d452e605aed4db43c2
KerunPan-uwindsor <pan16@uwindsor.ca>
2021-10-24 16:44:39 -0400

    build: add pgadmin into web.dev.yml, setup pgdb in setting.py and rename container

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'testapp',
 ]
 
@@ -79,8 +80,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'pgdb',
+        'PORT': 5432,
     }
 }
 
