f96877e25d64307c17162dacfdbe71eaaf41fa75
KerunPan-uwindsor <pan16@uwindsor.ca>
2021-10-31 15:47:03 -0400

    build: add python-decouple3.5 in requirements, and save SECRET_KEY in .env

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jggkl@^^zxwt!_oa_bv#x9(z+=#=$l9xke5wb24t4+kp_-l3k*'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
